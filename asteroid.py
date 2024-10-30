2999d392a1632dec238d4870ab2a08bff601a93e
Andreas J Mueller <andy@cnm.dev>
2024-10-30 14:07:54 -0600

    properly initialize Asteroid class

--- asteroid.py
+++ asteroid.py
@@ -4,11 +4,10 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.center = pygame.Vector2(x, y)
-        self.radius = radius
+        super().__init__(x, y, radius)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.center, self.radius, 2)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
     def update(self, dt):
-        self.center += self.velocity * dt
+        self.position += self.velocity * dt
