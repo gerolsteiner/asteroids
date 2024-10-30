f25433dd29dbdb75ff89159959a778a8446db5ca
Andreas J Mueller <andy@cnm.dev>
2024-10-30 13:33:29 -0600

    add rotation via a/d keys

--- main.py
+++ main.py
@@ -19,9 +19,12 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        player.update(dt)
+
         screen.fill((0,0,0))
         player.draw(screen)
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
